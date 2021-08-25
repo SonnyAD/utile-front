a919706c633dd2e13489c9f3405c3a3f292e784a
Sonny Alves Dias <sonny.alvesdias@pixelmatic.com>
2021-08-25 09:14:50 +0800

    Removes the remnant of the dart project in the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-ln -sf config.prod.dart web/config.dart
-
-webdev.bat build
-for i in {1..5}; do rsync -va --delete -e "ssh"  ./build/ root@server:/utile/front/ && break; done
-
-ln -sf config.dev.dart web/config.dart
+ rsync -va --delete -e "ssh"  ./public/ root@server:/utile/front/ 

cd95b002fdeffaf0f68b8237b492d3c79e3ad0c2
Sonny Alves Dias <sonny.alvesdias@pixelmatic.com>
2021-09-14 00:36:36 +0800

    Fixes the Links page

--- src/routes/links.svelte
+++ src/routes/links.svelte
@@ -1,12 +1,23 @@
 <script>
 	import { API_URL } from '$lib/Env.js';
+	import { onMount } from 'svelte';
 
-    const links = fetch(API_URL + '/links', {
-        method: 'GET',
-        headers: {
-				Accept: 'application/json'
+    const loadLinks = async () => {
+		const res = await fetch(API_URL + '/links', {
+			method: 'GET',
+			headers: {
+					Accept: 'application/json'
 			}        
-    });
+        });
+
+		return await res.json();
+	};
+
+	let links;
+
+	onMount(() => {
+		links = loadLinks();
+	});
 </script>
 
 <table style="table-layout: fixed; width: 100%">
@@ -18,17 +29,19 @@
 		</tr>
 	</thead>
 	<tbody>
-        {#await links}
-            ...
-        {:then data} 
-            {#each data.links as link}
-                <tr>
-                    <td><a href={link.url} title={link.description}>{link.url}</a></td>
-                    <td>{link.description}</td>
-                    <td>{link.tags.join(', ')}</td>
-                </tr>
-            {/each}
-        {/await}
+		{#if links}
+			{#await links}
+				...
+			{:then data} 
+				{#each data.links as link}
+					<tr>
+						<td><a href={link.url} title={link.description} target="_blank" rel="noopener">{link.url}</a></td>
+						<td><em>{link.description}</em></td>
+						<td><small>{link.tags.join(', ')}</small></td>
+					</tr>
+				{/each}
+			{/await}
+		{/if}
 	</tbody>
 	<tfoot>
 		<tr>
@@ -38,5 +51,13 @@
 </table>
 
 <style>
-	
+	tr>td:first-child, tr>th:first-child  {
+		width: 40%;
+	}
+	tr>td:nth-child(2), tr>th:nth-child(2)  {
+		width: 45%;
+	}
+	tr>td:nth-child(3), tr>th:nth-child(3)  {
+		width: 15%;
+	}
 </style>

dbf5c4eff373d05c43dec146fc98b1a1fd45164b
Sonny Alves Dias <sonny@alvesdi.as>
2024-07-01 15:14:12 +0800

    Fixes formatting

--- tests/password_test.js
+++ tests/password_test.js
@@ -4,7 +4,7 @@ test('password page works as expected', async ({ page }) => {
 	await page.goto('/password');
 	await expect(page.getByRole('heading', { name: 'Password Generator' })).toBeVisible();
 	await page.getByText('Generate', { exact: true }).click();
-    const result = page.locator('#result');
+	const result = page.locator('#result');
 	await expect(result).toBeVisible();
-    expect((await result.innerHTML()).length).toBeGreaterThanOrEqual(20);
+	expect((await result.innerHTML()).length).toBeGreaterThanOrEqual(20);
 });

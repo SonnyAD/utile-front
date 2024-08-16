a4e4ded34a1d212f75d82ffbb9c2d248c2f3e79e
Sonny Alves Dias <sonny@alvesdi.as>
2024-08-17 00:44:00 +0800

    Adds a page to consult some math constants

--- src/routes/math/+page.svelte
+++ src/routes/math/+page.svelte
@@ -3,35 +3,105 @@
 	import { notifier } from '$lib/notifications';
 	import { copy } from 'svelte-copy';
 	import { API_URL } from '$lib/Env.js';
+	import { onMount } from 'svelte';
 
 	/**
 	 * @type {Promise<string>}
 	 */
 	let output;
 
-	async function computePi() {
-		return compute('pi');
-	}
+	let websocketOutput = "";
 
-	async function computeTau() {
-		return compute('tau');
-	}
+	/**
+	 * @type {WebSocket}
+	 */
+	let conn;
+
+	let previousConstant = '';
+	let page = 0;
+
+
+	onMount(()=> {
+		conn = new WebSocket("ws://localhost:3000/api/math/ws");
+
+        conn.onclose = function (evt) {
+            console.log("Websocket connection lost: " + evt.reason)
+        };
+
+        conn.onmessage = function (evt) {
+			websocketOutput += evt.data
+			output = new Promise((resolve) => {
+				resolve(websocketOutput);
+			});
+        };
+	});	
 
 	/**
 	 * @param {string} constantName
 	 */
 	async function compute(constantName) {
-		const res = await fetch(API_URL + '/' + constantName, {
+		resetOutput();
+
+		const res = await fetch(API_URL + '/math/' + constantName, {
 			method: 'GET'
 		});
 
 		return await res.text();
 	}
 
+	function resetOutput() {
+		output = new Promise((resolve) => resolve(""));
+		websocketOutput = "";
+		previousConstant = '';
+	}
+
+	/**
+	 * @param {string} constantName
+	 */	
+	function requestPage(constantName) {
+		// Make sure websocket is on
+		if (!conn) {
+			return false;
+		}
+
+		// To swap between pi and tau
+		if (previousConstant == '' || previousConstant != constantName) {
+			resetOutput();
+			previousConstant = constantName;
+			page = 0;
+		} else {
+			page++;
+		}
+
+		// send page request, page size 1000
+		conn.send(constantName + " " + page.toString() + ", 1000")
+	}
+
+	function nextPage() {
+		requestPage(previousConstant);
+	}
+
+	// Lazy-loading logic
+	let y = 0;
+	let innerHeight = 0;
+	let clientHeight = 0;
+
+	$: {
+		if (previousConstant != '') {
+			const scrolledTo = y + innerHeight;
+			const threshold = 30;
+			const hasReachedBottom = clientHeight - threshold <= scrolledTo;
+			if (hasReachedBottom) 
+				nextPage();
+		}		
+	};
+
 	const copied = () => {
 		notifier.success('Value copied to clipboard!');
 	};
 </script>
+
+<svelte:window bind:scrollY={y} bind:innerHeight={innerHeight} />
 
 <Header
 	title="Some mathematics stuff"
@@ -39,31 +109,41 @@
 />
 
 <p>
-	<button class="w3-button w3-ripple w3-theme w3-round" on:click={() => (output = computePi())}
+	<button class="w3-button w3-ripple w3-theme w3-round" on:click={() => (output = compute('pi'))}
 		>Compute &pi; 10K first decimals</button
 	>
-	<button class="w3-button w3-ripple w3-theme w3-round" on:click={() => (output = computeTau())}
-		>Generate &tau; 10K first decimals</button
+	<button class="w3-button w3-ripple w3-theme w3-round" on:click={() => (output = compute('tau'))}
+		>Compute &tau; 10K first decimals</button
 	>
 </p>
-
-<p title="result" style="word-break: break-all;">
-	{#if output}
-		{#await output}
-			...
-		{:then data}
-			<button class="w3-button w3-round grey" use:copy={data} on:svelte-copy={() => copied()}>
-				Copy</button
-			>
-			<br /> <br />
-			{data}
-		{:catch error}
-			Failed: {error.message}
-		{/await}
-	{:else}
-		&nbsp;
-	{/if}
+<p>
+	<button class="w3-button w3-ripple w3-theme w3-round" on:click={() => (requestPage('pi'))}
+		>Lazy load &pi; 1M first decimals</button
+	>
+	<button class="w3-button w3-ripple w3-theme w3-round" on:click={() => (requestPage('tau'))}
+		>Lazy load &tau; 1M first decimals</button
+	>	
 </p>
+
+<div bind:clientHeight={clientHeight}>
+	<p title="result" style="word-break: break-all;">
+		{#if output}
+			{#await output}
+				...
+			{:then data}
+				<button class="w3-button w3-round grey" use:copy={data} on:svelte-copy={() => copied()}>
+					Copy</button
+				>
+				<br /> <br />
+				{data}
+			{:catch error}
+				Failed: {error.message}
+			{/await}
+		{:else}
+			&nbsp;
+		{/if}
+	</p>
+</div>
 
 <style>
 	button.grey {
